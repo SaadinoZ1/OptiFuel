c78394acf2f6e3600e4ba734419778bc49b78e11
SaadinoZ1 <shicham230@gmail.com>
2024-07-31 13:27:04 +0100

    Add CRUD operations for Contact

---
+++ OptiFuel/Controllers/CommissionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OptiFuel.Data;
+
+namespace OptiFuel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommissionController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+    }
+}
---
+++ OptiFuel/Controllers/ContactController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OptiFuel.Data;
+using OptiFuel.Models;
+
+namespace OptiFuel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public ContactController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contact>>> GetContacts()
+        {
+            return await _appDbContext.contacts.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contact>> GetContact(Guid id)
+        {
+            var contact = await _appDbContext.contacts.FindAsync(id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return contact;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Contact>> PostContact(Contact contact)
+        {
+            contact.Id = Guid.NewGuid();
+            contact.e_created_on = DateTime.UtcNow;
+            _appDbContext.contacts.Add(contact);
+            await _appDbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetContact), new { id = contact.Id }, contact);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutContact(Guid id, Contact contact)
+        {
+            if (id != contact.Id)
+            {
+                return BadRequest();
+            }
+
+            contact.e_updated_on = DateTime.UtcNow;
+            _appDbContext.Entry(contact).State = EntityState.Modified;
+
+            try
+            {
+                await _appDbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContactExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteContact(Guid id)
+        {
+            var contact = await _appDbContext.contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            _appDbContext.contacts.Remove(contact);
+            await _appDbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ContactExists(Guid id)
+        {
+            return _appDbContext.contacts.Any(e => e.Id == id);
+        }
+    }
+
+
+}
+
